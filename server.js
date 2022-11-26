7f9bb9ef9dfa107c42cee80b0a8165a85b0d2535
Your Name <you@example.com>
2022-11-26 15:12:04 -0500

    added import dependencies, middleware, and listening port

---
+++ server.js
@@ -0,0 +1,42 @@
+//-------------------------------------------
+// Import Our Dependencies
+//-------------------------------------------
+require("dotenv").config() // Load ENV Variables
+const express = require("express") // bring in express to make our app.
+const morgan = require("morgan") //nice logger for our request
+const methodOverride = require("method-override") // allows us to override post request from our ejs/forms
+
+const PORT = process.env.PORT || 3001; // in case of env file missing
+const app = express()
+
+//-------------------------------------------
+// Middleware
+//-------------------------------------------
+
+app.use(morgan("tiny")) //logging
+app.use(methodOverride("_method")) // override for put and delete requests from forms
+app.use(express.urlencoded({extended: true})) // parse urlencoded request
+
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+
+
+//-------------------------------------------
+// Import Our Dependencies
+//-------------------------------------------
+
+// require("dotenv").config() // Load ENV Variables
+// const express = require("express") // bring in express to make our app.
+// const morgan = require("morgan") //nice logger for our request
+// const methodOverride = require("method-override") // allows us to override post request from our ejs/forms
+// const FruitRouter = require("./controllers/fruits")
+
+// const PORT = process.env.PORT
+// const app = express()
+
+//----------------------------------------
+//  Middleware
+//----------------------------------------
+// app.use(morgan("tiny")) //logging
+// app.use(methodOverride("_method")) // override for put and delete requests from forms
+// app.use(express.urlencoded({extended: true})) // parse urlencoded request
